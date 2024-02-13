394a0e0a64bbffe84021f964b50d9780939515f5
brianheineman <brian.heineman@gmail.com>
2024-02-12 19:56:04 -0700

    bug: correct bug where serialization fails when there is a draft release of the PostgreSQL binaries

--- postgresql_archive/src/github.rs
+++ postgresql_archive/src/github.rs
@@ -1,6 +1,5 @@
 //! Structs for GitHub API responses
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub(crate) struct Release {
@@ -9,41 +8,11 @@ pub(crate) struct Release {
     pub upload_url: String,
     pub html_url: String,
     pub id: i64,
-    pub author: Author,
-    pub node_id: String,
     pub tag_name: String,
-    pub target_commitish: String,
     pub name: String,
     pub draft: bool,
     pub prerelease: bool,
-    pub created_at: String,
-    pub published_at: String,
     pub assets: Vec<Asset>,
-    pub tarball_url: String,
-    pub zipball_url: String,
-    pub body: Option<Value>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub(crate) struct Author {
-    pub login: String,
-    pub id: i64,
-    pub node_id: String,
-    pub avatar_url: String,
-    pub gravatar_id: String,
-    pub url: String,
-    pub html_url: String,
-    pub followers_url: String,
-    pub following_url: String,
-    pub gists_url: String,
-    pub starred_url: String,
-    pub subscriptions_url: String,
-    pub organizations_url: String,
-    pub repos_url: String,
-    pub events_url: String,
-    pub received_events_url: String,
-    pub r#type: String,
-    pub site_admin: bool,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -53,12 +22,8 @@ pub(crate) struct Asset {
     pub node_id: String,
     pub name: String,
     pub label: String,
-    pub uploader: Author,
     pub content_type: String,
     pub state: String,
     pub size: i64,
-    pub download_count: i64,
-    pub created_at: String,
-    pub updated_at: String,
     pub browser_download_url: String,
 }
