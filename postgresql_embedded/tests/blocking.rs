321150e43df928e760cbe159b6dea26322ac31a9
brianheineman <brian.heineman@gmail.com>
2024-02-13 09:01:10 -0700

    test: improve lifecycle test coverage

--- postgresql_embedded/tests/blocking.rs
+++ postgresql_embedded/tests/blocking.rs
@@ -9,6 +9,11 @@ use test_log::test;
 #[test]
 fn test_lifecycle() -> Result<()> {
     let mut postgresql = PostgreSQL::default();
+    let settings = postgresql.settings();
+
+    // Verify that an ephemeral instance is created by default
+    assert_eq!(0, settings.port);
+    assert!(settings.temporary);
 
     let initial_statuses = [Status::NotInstalled, Status::Installed, Status::Stopped];
     assert!(initial_statuses.contains(&postgresql.status()));
@@ -20,6 +25,7 @@ fn test_lifecycle() -> Result<()> {
     assert_eq!(Status::Started, postgresql.status());
 
     let database_name = "test";
+    assert!(!postgresql.database_exists(database_name)?);
     postgresql.create_database(database_name)?;
     assert!(postgresql.database_exists(database_name)?);
     postgresql.drop_database(database_name)?;
--- postgresql_embedded/tests/postgresql.rs
+++ postgresql_embedded/tests/postgresql.rs
@@ -6,6 +6,11 @@ use test_log::test;
 
 async fn lifecycle() -> Result<()> {
     let mut postgresql = PostgreSQL::default();
+    let settings = postgresql.settings();
+
+    // Verify that an ephemeral instance is created by default
+    assert_eq!(0, settings.port);
+    assert!(settings.temporary);
 
     let initial_statuses = [Status::NotInstalled, Status::Installed, Status::Stopped];
     assert!(initial_statuses.contains(&postgresql.status()));
@@ -17,6 +22,7 @@ async fn lifecycle() -> Result<()> {
     assert_eq!(Status::Started, postgresql.status());
 
     let database_name = "test";
+    assert!(!postgresql.database_exists(database_name).await?);
     postgresql.create_database(database_name).await?;
     assert!(postgresql.database_exists(database_name).await?);
     postgresql.drop_database(database_name).await?;
