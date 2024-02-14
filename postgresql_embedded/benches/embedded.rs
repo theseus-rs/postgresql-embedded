0302cb4d3a2b2580cf6e242c6a480b7523cb583a
brianheineman <brian.heineman@gmail.com>
2024-02-14 09:59:19 -0700

    test: add benchmarks

---
+++ postgresql_embedded/benches/embedded.rs
@@ -0,0 +1,33 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use postgresql_embedded::blocking::PostgreSQL;
+use postgresql_embedded::Result;
+use std::time::Duration;
+
+fn benchmarks(criterion: &mut Criterion) {
+    bench_lifecycle(criterion).ok();
+}
+
+fn bench_lifecycle(criterion: &mut Criterion) -> Result<()> {
+    criterion.bench_function("extract", |bencher| {
+        bencher.iter(|| {
+            lifecycle().ok();
+        });
+    });
+
+    Ok(())
+}
+
+fn lifecycle() -> Result<()> {
+    let mut postgresql = PostgreSQL::default();
+    postgresql.setup()?;
+    postgresql.start()?;
+    postgresql.stop()
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(60));
+    targets = benchmarks
+);
+criterion_main!(benches);
