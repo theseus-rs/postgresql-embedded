8d75092f6c9b748df59518fc7826791876d70dd7
brianheineman <brian.heineman@gmail.com>
2024-06-28 11:02:40 -0600

    test: remove extraneous tests

--- postgresql_archive/tests/archive.rs
+++ postgresql_archive/tests/archive.rs
@@ -5,41 +5,6 @@ use semver::VersionReq;
 use std::fs::{create_dir_all, remove_dir_all};
 use test_log::test;
 
-async fn test_get_archive_for_version_constant(major: u64) -> anyhow::Result<()> {
-    let version_req = VersionReq::parse(&format!("={major}"))?;
-    let (archive_version, _archive) = get_archive(DEFAULT_POSTGRESQL_URL, &version_req).await?;
-
-    assert!(version_req.matches(&archive_version));
-    assert_eq!(major, archive_version.major);
-    Ok(())
-}
-
-#[test(tokio::test)]
-async fn test_get_archive_for_version_constant_v16() -> anyhow::Result<()> {
-    test_get_archive_for_version_constant(16).await
-}
-
-#[test(tokio::test)]
-async fn test_get_archive_for_version_constant_v15() -> anyhow::Result<()> {
-    test_get_archive_for_version_constant(15).await
-}
-
-#[test(tokio::test)]
-async fn test_get_archive_for_version_constant_v14() -> anyhow::Result<()> {
-    test_get_archive_for_version_constant(14).await
-}
-
-#[test(tokio::test)]
-async fn test_get_archive_for_version_constant_v13() -> anyhow::Result<()> {
-    test_get_archive_for_version_constant(13).await
-}
-
-#[test(tokio::test)]
-#[allow(deprecated)]
-async fn test_get_archive_for_version_constant_v12() -> anyhow::Result<()> {
-    test_get_archive_for_version_constant(12).await
-}
-
 #[test(tokio::test)]
 async fn test_get_version_not_found() -> postgresql_archive::Result<()> {
     let invalid_version_req = VersionReq::parse("=1.0.0")?;
@@ -51,7 +16,7 @@ async fn test_get_version_not_found() -> postgresql_archive::Result<()> {
 
 #[test(tokio::test)]
 async fn test_get_version() -> anyhow::Result<()> {
-    let version_req = VersionReq::STAR;
+    let version_req = VersionReq::parse("=16.3.0")?;
     let latest_version = get_version(DEFAULT_POSTGRESQL_URL, &version_req).await?;
 
     assert!(version_req.matches(&latest_version));
