3d51a8d928c7fe4d8b4bb66d9c7b831b2ca49cc2
brianheineman <brian.heineman@gmail.com>
2024-07-05 19:40:06 -0600

    test: increase timeout to 30 seconds

--- postgresql_embedded/src/settings.rs
+++ postgresql_embedded/src/settings.rs
@@ -289,7 +289,7 @@ mod tests {
                 .url("test")
                 .replace(settings.password.as_str(), "password")
         );
-        assert_eq!(Some(Duration::from_secs(5)), settings.timeout);
+        assert_eq!(Some(Duration::from_secs(30)), settings.timeout);
         assert!(settings.configuration.is_empty());
     }
 
