bd8b96fbb82b602c70a3f7e7cf6cc93fc94916bb
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2024-08-17 22:33:09 +0200

    Adding creating a post, example coming from https://diesel.rs/guides/getting-started

--- examples/embedded_async_diesel_r2d2/src/main.rs
+++ examples/embedded_async_diesel_r2d2/src/main.rs
@@ -3,34 +3,64 @@
 
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use postgresql_embedded::{PostgreSQL, Result, Settings, VersionReq};
+mod models;
+pub mod schema;
 
+use self::models::*;
+use diesel::prelude::*;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations/");
 #[tokio::main]
 async fn main() -> Result<()> {
     let settings = Settings {
         version: VersionReq::parse("=16.4.0")?,
+        username: "postgres".to_string(),
+        password: "postgres".to_string(),
         ..Default::default()
     };
     let mut postgresql = PostgreSQL::new(settings);
     postgresql.setup().await?;
     postgresql.start().await?;
 
-    let database_name = "test";
+    let database_name = "diesel_demo";
     postgresql.create_database(database_name).await?;
     postgresql.database_exists(database_name).await?;
 
     {
         let database_url = postgresql.settings().url(database_name);
         let manager = ConnectionManager::<PgConnection>::new(database_url);
-        let _pool = Pool::builder()
+        let pool = Pool::builder()
             .test_on_check_out(true)
             .build(manager)
             .expect("Could not build connection pool");
+        let mut mig_run = pool.clone().get().unwrap();
+        mig_run.run_pending_migrations(MIGRATIONS).unwrap();
+
+        let post = create_post(
+            &mut pool.get().unwrap(),
+            "My First Post",
+            "This is my firs post",
+        );
+        println!("Post '{}' created", post.title);
     }
 
     postgresql.drop_database(database_name).await?;
 
     postgresql.stop().await
+}
+
+pub fn create_post(conn: &mut PgConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
 }
 
 #[cfg(test)]
---
+++ examples/embedded_async_diesel_r2d2/src/models.rs
@@ -0,0 +1,18 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::posts)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+}
