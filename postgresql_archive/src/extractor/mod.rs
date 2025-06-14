87888747c4603e3fd48c4631fd4223baf46684a6
Gerd Zellweger <mail@gerdzellweger.com>
2025-06-14 15:50:45 -0700

    Make liblzma an optional dependency.
    It is only used when extracting binaries from the zonky repo
    which is already behind a feature flag.
    
    See also #187 for an xplanation of why this cause issues when
    a project also pulls in the xz2 crate (can't link the lzma C
    library twice).
    
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- postgresql_archive/src/extractor/mod.rs
+++ postgresql_archive/src/extractor/mod.rs
@@ -1,10 +1,12 @@
 mod model;
 pub mod registry;
 mod tar_gz_extractor;
+#[cfg(feature = "liblzma")]
 mod tar_xz_extractor;
 mod zip_extractor;
 
 pub use model::ExtractDirectories;
 pub use tar_gz_extractor::extract as tar_gz_extract;
+#[cfg(feature = "liblzma")]
 pub use tar_xz_extractor::extract as tar_xz_extract;
 pub use zip_extractor::extract as zip_extract;
