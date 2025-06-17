60cd2a366bccc5bdd9f62dd9652cf55b5a0160f0
brianheineman <brian.heineman@gmail.com>
2025-06-17 08:31:30 -0600

    feat: add extractor feature flags

--- postgresql_archive/src/extractor/mod.rs
+++ postgresql_archive/src/extractor/mod.rs
@@ -1,12 +1,16 @@
 mod model;
 pub mod registry;
+#[cfg(feature = "tar-gz")]
 mod tar_gz_extractor;
-#[cfg(feature = "liblzma")]
+#[cfg(feature = "tar-xz")]
 mod tar_xz_extractor;
+#[cfg(feature = "zip")]
 mod zip_extractor;
 
 pub use model::ExtractDirectories;
+#[cfg(feature = "tar-gz")]
 pub use tar_gz_extractor::extract as tar_gz_extract;
-#[cfg(feature = "liblzma")]
+#[cfg(feature = "tar-xz")]
 pub use tar_xz_extractor::extract as tar_xz_extract;
+#[cfg(feature = "zip")]
 pub use zip_extractor::extract as zip_extract;
